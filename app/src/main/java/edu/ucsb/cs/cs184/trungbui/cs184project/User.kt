901623774dc56892b1893b5cb531197af1ecb424
Trung Bui <trungbui@ucsb.edu>
2022-06-02 09:51:24 -0700

    push for eric

---
+++ app/src/main/java/edu/ucsb/cs/cs184/trungbui/cs184project/User.kt
@@ -0,0 +1,8 @@
+package edu.ucsb.cs.cs184.trungbui.cs184project
+
+class User(
+    val firstName: String,
+    val lastName: String,
+    var score: Int,
+    val email: String
+) { /*...*/ }
--- app/src/main/java/edu/ucsb/cs/cs184/trungbui/cs184project/ui/leaderboard/LeaderboardFragment.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/trungbui/cs184project/ui/leaderboard/LeaderboardFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import edu.ucsb.cs.cs184.trungbui.cs184project.R
+import edu.ucsb.cs.cs184.trungbui.cs184project.User
 import edu.ucsb.cs.cs184.trungbui.cs184project.databinding.FragmentLeaderboardBinding
+
 
 class LeaderboardFragment : Fragment() {
 
@@ -16,6 +21,7 @@ class LeaderboardFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var leaderboardListView: ListView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,10 +34,23 @@ class LeaderboardFragment : Fragment() {
         _binding = FragmentLeaderboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        // Fetching the leaderboard list view
+//        leaderboardListView = requireView().findViewById<ListView>(R.id.leaderboard_list_view)
+//
+//        val recipeList = Array(3) { index ->
+//            User("firstName${index + 1}", "lastName${index + 1}", index + 1, "email${index + 1}")
+//        }
+//
+//        val listItems = arrayOfNulls<String>(recipeList.size)
+//
+//        for (i in 0 until recipeList.size) {
+//            val recipe = recipeList[i]
+//            listItems[i] = recipe.email
+//        }
+//
+//        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, listItems)
+//        leaderboardListView.adapter = adapter
+
         return root
     }
 
