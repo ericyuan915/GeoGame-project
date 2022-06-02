8fc1786373fe90f2223e17a7bf6fa28795d8ea97
Trung Bui <trungbui@ucsb.edu>
2022-06-02 09:21:55 -0700

    added-gallery-fragment

--- app/src/main/java/edu/ucsb/cs/cs184/trungbui/cs184project/MainActivity.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/trungbui/cs184project/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_multichoice, R.id.nav_google_maps, R.id.nav_results
+                R.id.nav_home, R.id.nav_leaderboard, R.id.nav_multichoice, R.id.nav_google_maps, R.id.nav_results
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/edu/ucsb/cs/cs184/trungbui/cs184project/ui/leaderboard/LeaderboardFragment.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/trungbui/cs184project/ui/leaderboard/LeaderboardFragment.kt
@@ -1,4 +1,4 @@
-package edu.ucsb.cs.cs184.trungbui.cs184project.ui.gallery
+package edu.ucsb.cs.cs184.trungbui.cs184project.ui.leaderboard
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import edu.ucsb.cs.cs184.trungbui.cs184project.databinding.FragmentGalleryBinding
+import edu.ucsb.cs.cs184.trungbui.cs184project.databinding.FragmentLeaderboardBinding
 
-class GalleryFragment : Fragment() {
+class LeaderboardFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentLeaderboardBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,9 +23,9 @@ class GalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+            ViewModelProvider(this).get(LeaderboardViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentLeaderboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
